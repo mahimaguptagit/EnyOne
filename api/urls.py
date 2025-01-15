73395fc282346bfdb7a510f624e2de0fdbfd00e7
Your Name <youremail@example.com>
2025-01-15 12:06:33 +0530

    update

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('user-login/',views.UserLoginView.as_view(),name='UserLogin'),
-    path('user-profile-detail/',views.UserProfileView.as_view(),name='UserProfile')
+    path('user-profile-detail/',views.UserProfileView.as_view(),name='UserProfile'),
+    path('user-add-raise-ticket/',views.UserRaiseTicketView.as_view(),name='UserRaiseTicket')
     
 ]
--- api/views.py
+++ api/views.py
@@ -19,30 +19,42 @@ class UserLoginView(APIView):
         username=request.data.get('username')
         password=request.data.get('password')
         if not username or not password:
-            return Response({'status':'False','message':'Both username and password are required.'})
+            return Response({'status':'false','message':'Both username and password are required.'})
         user = authenticate(request=request,username=username,password=password)
         if user:
             login(request,user)
             token=get_tokens_for_user(user)
-            return Response({'status':'True','access_token':token,'message':'LogIn Successfully'})
-        return Response({'status':'False','message':'Check UserName or Password !!'})
+            return Response({'status':'true','access_token':token,'message':'LogIn Successfully'})
+        return Response({'status':'false','message':'Check UserName or Password !!'})
     
 class UserProfileView(APIView):
     permission_classes = [IsAuthenticated]
     def get(self, request,format=None):
-        userdata=User.objects.filter(id=request.user.id).first()
-        userdetail={
-            "username":userdata.username,
-            "first_name":userdata.first_name,
-            "last_name":userdata.last_name,
-            "email":userdata.email,
-            "image":userdata.image.url,
-            "phone_number":userdata.phone_number,}
-        return Response({'status':'True','message':'User Profile Data','user_data':userdetail})
+        try:
+            userdata=User.objects.filter(id=request.user.id).first()
+            userdetail={
+                "username":userdata.username,
+                "first_name":userdata.first_name,
+                "last_name":userdata.last_name,
+                "email":userdata.email,
+                "image":userdata.image.url,
+                "phone_number":userdata.phone_number,}
+            return Response({'status':'true','message':'User Profile Data','user_data':userdetail})
+        except User.DoesNotExist:
+            return Response({'status':'false','message':'User data not found !!'})
 
 
-class UserRaiseTicket(View):
-    def post(self,request):
+class UserRaiseTicketView(View):
+    permission_classes=[IsAuthenticated]
+    def post(self,request,format=None):
+        userdata=request.user
+        ticket_type=request.data.get('ticket_type')
+        ticket_title=request.data.get('ticket_title')
+        ticket_description=request.data.get('ticket_description')
+        ticket_number=request.data.get('ticket_number')
+        priority_level=request.data.get('priority_level')
+
+        ['user','ticket_type','ticket_title','ticket_description','ticket_number','priority_level','ticket_file','submission_status']
        
         pass
 
