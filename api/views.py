36ef2b14298bbcf3d5380d2ed8cdf3a7d6c44ef3
Your Name <youremail@example.com>
2025-01-22 11:23:06 +0530

    update

--- api/views.py
+++ api/views.py
@@ -48,44 +48,15 @@ class UserProfileView(APIView):
             return Response({'status':'true','message':'User Profile Data','user_data':userdetail})
         except User.DoesNotExist:
             return Response({'status':'false','message':'User data not found !!'})
-
-
-# class UserRaiseTicketView(APIView):
-#     permission_classes=[IsAuthenticated]
-#     def post(self,request,format=None):
-#         userdata=request.user
-#         serializer = TicketSerializer(data=request.data)
-#         if serializer.is_valid():
-#             data = serializer.validated_data
-#             try:
-#                 user=request.user
-#                 userdata=User.objects.filter(id=user.id)
-#                 ticket_type=data['ticket_type']
-#                 ticket_title=data['ticket_title']
-#                 priority_level=data['priority_level']
-#                 ticket_description=data['ticket_description']
-#                 ticket_file=data['ticket_file']
-#                 booking_id = Ticket.objects.latest('id').id +1 if Ticket.objects.exists() else 1
-#                 timestamp_part = datetime.now().strftime('%y%m%d%H%M%S%f')[:-3]
-#                 ticket_number = f"ENYONE0260{booking_id}{timestamp_part}"
-#                 ticket_data=Ticket.objects.create(user=userdata,ticket_type=ticket_type,ticket_title=ticket_title,ticket_description=ticket_description,ticket_number=ticket_number,priority_level=priority_level,ticket_file=ticket_file)
-#                 return Response({'status':'true','message':'Ticket Raise Successfully'})
-#             except User.DoesNotExist:
-#                 return Response({'status':'false','message':'User not Found'})
     
 class UserRaiseTicketView(APIView):
     permission_classes = [IsAuthenticated]
     def post(self, request, format=None):
-        # Get the logged-in user
         user = request.user
         userdata=User.objects.filter(is_superuser=False,id=user.id).first()
-
-        # Deserialize and validate the request data
         serializer = TicketSerializer(data=request.data)
         if serializer.is_valid():
             data = serializer.validated_data
-
-            # Extract data from validated serializer
             ticket_type = data.get('ticket_type')
             ticket_title = data.get('ticket_title')
             priority_level = data.get('priority_level')
@@ -96,12 +67,9 @@ class UserRaiseTicketView(APIView):
                 return Response({'status':'false','message':'Check Required Fields !!'})
 
             try:
-                # Generate ticket number
                 latest_ticket_id = Ticket.objects.latest('id').id + 1 if Ticket.objects.exists() else 1
                 timestamp_part = datetime.now().strftime('%y%m%d%H%M%S%f')[:-3]
                 ticket_number = f"ENYONE0{latest_ticket_id}0{timestamp_part}"
-
-                # Create ticket record
                 ticket_data = Ticket.objects.create(
                     user=userdata,
                     ticket_type=ticket_type,
@@ -121,7 +89,7 @@ class UserRaiseTicketView(APIView):
     
 
 class ShowRaisedTicketDataView(APIView):
-    permission_classes=[IsAuthenticated]
+    permission_classes = [IsAuthenticated]
     def post(self,request):
         ticket_type=request.data.get('ticket_type')
         if ticket_type:
@@ -149,7 +117,7 @@ class ShowRaisedTicketDataView(APIView):
         return Response({'status':'true','message':'Rised Ticket Data','ticket_details':ticketdetails})
     
 class ShowParticularTicketDrtailsView(APIView):
-    permission_classes=[IsAuthenticated]
+    permission_classes = [IsAuthenticated]
     def post(self,request):
         ticket_id=request.data.get('ticket_id')
         if ticket_id:
@@ -173,6 +141,21 @@ class ShowParticularTicketDrtailsView(APIView):
             return Response({'status':'true','message':'Ticket Details !!','ticketdetail':ticket_details})
         else:
             return Response({'status':'false','message':'Ticket Not Found '})
+        
+class AddSatisfactionScoreView(APIView):
+    permission_classes = [IsAuthenticated]
+    def post(self,request):
+        ticket_id=request.data.get('ticket_id')
+        satisfaction_score=request.data.get('satisfaction_score')
+        if not ticket_id and not satisfaction_score:
+            return Response({'status':'false','message':'Please add required fields'})
+        try:
+            ticket_data=Ticket.objects.filter(id=ticket_id,user=request.user).first()
+            ticket_data.satisfaction_score=satisfaction_score
+            ticket_data.save()
+            return Response({'status':'true','message':'Satisfaction Score Updated Successfully'})
+        except Ticket.DoesNotExist :
+            return Response({'status':'false','message':'Ticket Not Found'})
 
 
     
